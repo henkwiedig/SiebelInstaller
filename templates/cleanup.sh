215d000d0b8eff465956caf0ec867090d8a3f78f
Henk Wiedig <henk@wiedig.de>
2013-11-10 13:03:27 +0100

    fix for cleanup script

--- templates/cleanup.sh
+++ templates/cleanup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-source $SCRIPT_ROOT/config.defaults
-if [ -e $SCRIPT_ROOT/config.local ]
+FULL_PATH=`readlink -f $0 ` 
+SCRIPT_ROOT=`dirname $FULL_PATH`
+
+source $SCRIPT_ROOT/../config.defaults
+
+if [ -e $SCRIPT_ROOT/../config.local ]
 then
-  source $SCRIPT_ROOT/config.local
+  source $SCRIPT_ROOT/../config.local
 fi
 
+
 #remove oracle
-rm -fr $ORACLE_BASE /etc/oraInst.loc /etc/oratab 
+rm -fr $ORACLE_BASE /etc/oraInst.loc /etc/oratab /etc/init.d/dbora
 userdel -f -r oracle
 groupdel oinstall
 groupdel dba 
@@ -15,6 +20,7 @@ groupdel oper
 rm -f .status/download_from_mos_oracle_11.2.0.3 .status/install_oracle_oracle_11.2.0.3 .status/unpack_product_oracle_11.2.0.3
 
 #remove siebel
+rm /etc/init.d/siebel_services
 rm -rf $SIEBEL_BASE
 userdel -f -r siebel
 groupdel siebel
